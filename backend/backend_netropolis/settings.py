bf5838f63c342c034b26d520da48ccf8483df0e6
Pranay Pandey <pranaypandeyofficial@gmail.com>
2024-02-14 22:16:29 +0530

    config

--- backend/backend_netropolis/settings.py
+++ backend/backend_netropolis/settings.py
@@ -135,7 +135,8 @@ SWAGGER_SETTINGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
